ae2c5edfd1ab472b596b94f824549a56e9f17f71
L4Ph <me@l4ph.moe>
2025-07-08 22:05:31 +0900

    縦書きのパフォーマンスをチューニング

--- lib/widgets/tategaki.dart
+++ lib/widgets/tategaki.dart
@@ -3,7 +3,8 @@ import 'package:novelty/utils/vertical_rotated.dart';
 
 class Tategaki extends StatelessWidget {
   const Tategaki(
-    this.text, {
+    this.text,
+    {
     super.key,
     this.style,
     this.space = 12,
@@ -15,35 +16,114 @@ class Tategaki extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final splitText = text.split('\n');
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        for (final s in splitText) _textBox(s.runes),
-      ],
-    );
-  }
-
-  Widget _textBox(Runes runes) {
-    return Wrap(
-      direction: Axis.vertical,
-      children: [
-        for (final rune in runes)
-          Row(
-            children: [
-              SizedBox(width: space),
-              _character(String.fromCharCode(rune)),
-            ],
-          ),
-      ],
-    );
-  }
-
-  Widget _character(String char) {
-    if (VerticalRotated.map[char] != null) {
-      return Text(VerticalRotated.map[char]!, style: style);
-    } else {
-      return Text(char, style: style);
+    if (text.isEmpty) {
+      return const SizedBox.shrink();
     }
+
+    final effectiveTextStyle = style ?? DefaultTextStyle.of(context).style;
+    final painter = _TategakiPainter(
+      text: text,
+      textStyle: effectiveTextStyle,
+      space: space,
+    );
+
+    return CustomPaint(
+      size: painter.size,
+      painter: painter,
+    );
+  }
+}
+
+class _TategakiPainter extends CustomPainter {
+  _TategakiPainter({
+    required this.text,
+    required this.textStyle,
+    required this.space,
+  }) {
+    _calculateMetrics();
+  }
+
+  final String text;
+  final TextStyle textStyle;
+  final double space;
+
+  late final List<List<TextPainter>> _linesPainters;
+  late final List<double> _lineWidths;
+  late final Size size;
+
+  void _calculateMetrics() {
+    _linesPainters = [];
+    _lineWidths = [];
+    var totalWidth = 0.0;
+    var maxHeight = 0.0;
+
+    final lines = text.split('\n');
+
+    for (final line in lines) {
+      if (line.isEmpty) {
+        _lineWidths.add(0);
+        _linesPainters.add([]);
+        continue;
+      }
+
+      final charPainters = <TextPainter>[];
+      var maxCharWidth = 0.0;
+      var currentHeight = 0.0;
+
+      for (final char in line.runes) {
+        final character = String.fromCharCode(char);
+        final rotatedChar = VerticalRotated.map[character] ?? character;
+
+        final textPainter = TextPainter(
+          text: TextSpan(text: rotatedChar, style: textStyle),
+          textDirection: TextDirection.ltr,
+        )..layout();
+        charPainters.add(textPainter);
+
+        if (textPainter.width > maxCharWidth) {
+          maxCharWidth = textPainter.width;
+        }
+        currentHeight += textPainter.height;
+      }
+
+      _lineWidths.add(maxCharWidth);
+      _linesPainters.add(charPainters);
+      totalWidth += maxCharWidth + space;
+      if (currentHeight > maxHeight) {
+        maxHeight = currentHeight;
+      }
+    }
+
+    size = Size(totalWidth, maxHeight);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var nextLineLeftBoundary = size.width;
+
+    for (var i = 0; i < _linesPainters.length; i++) {
+      final charPainters = _linesPainters[i];
+      final lineWidth = _lineWidths[i];
+
+      final lineTotalWidth = charPainters.isEmpty ? 0 : lineWidth + space;
+      final currentLineLeftBoundary = nextLineLeftBoundary - lineTotalWidth;
+
+      if (charPainters.isNotEmpty) {
+        var dy = 0.0;
+        for (final textPainter in charPainters) {
+          textPainter.paint(canvas, Offset(currentLineLeftBoundary + space, dy));
+          dy += textPainter.height;
+        }
+      }
+
+      nextLineLeftBoundary = currentLineLeftBoundary;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant _TategakiPainter oldDelegate) {
+    return text != oldDelegate.text ||
+        textStyle != oldDelegate.textStyle ||
+        space != oldDelegate.space;
   }
 }
