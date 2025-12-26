937af16c1d15bd04aedb4264c1bdffa18d1b9186
L4Ph <me@l4ph.moe>
2025-12-27 02:24:20 +0900

    もろもろを修正

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://novelty.l4ph.moe",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
