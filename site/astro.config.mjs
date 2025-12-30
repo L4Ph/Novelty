e631edfeb25114522c8e8319e674566851024741
L4Ph <me@l4ph.moe>
2025-12-31 02:19:06 +0900

    Scoped CSS + Lightning CSSに移行

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://novelty.l4ph.moe",
   integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()],
     css: {
       transformer: "lightningcss",
     },
